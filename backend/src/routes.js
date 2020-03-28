b1e1328e5041824322403ce90925b11332d31930
Felipe Bessa <felipe-bessa@outlook.com>
2020-03-28 19:26:06 -0300

    Adicionando as rotas para gerenciar os Casos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
 const OngCtrl = require('./controllers/OngCtrl');
+const IncidentCtrl = require('./controllers/IncidentCtrl');
 
 const routes = express.Router();
 
 routes.post('/ongs', OngCtrl.create);
 routes.get('/ongs', OngCtrl.read);
+
+routes.post('/incidents', IncidentCtrl.create);
+routes.get('/incidents', IncidentCtrl.read);
+routes.delete('/incidents/:id', IncidentCtrl.delete);
 
 module.exports = routes;
