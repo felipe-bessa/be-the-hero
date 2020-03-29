0f4c60c4bdd0ab953a54ac5b8e2e0f827355dd14
Felipe Bessa <felipe-bessa@outlook.com>
2020-03-28 23:42:01 -0300

    Adicionando rota para autenticação da ONG

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
+const SessionCtrl = require('./controllers/SessionCtrl');
 const OngCtrl = require('./controllers/OngCtrl');
 const IncidentCtrl = require('./controllers/IncidentCtrl');
 const ProfileCtrl = require('./controllers/ProfileCtrl');
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionCtrl.create);
 
 routes.post('/ongs', OngCtrl.create);
 routes.get('/ongs', OngCtrl.read);
