78bca05b857816d67f2ed7837591d271396c5939
Felipe Bessa <felipe-bessa@outlook.com>
2020-04-11 04:28:23 -0300

    Configurando envio de parametros para a pagina de detalhes

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -11,17 +11,36 @@ import styles from './styles';
 
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const[loading, setLoading] = useState(false);
+
   const navigation = useNavigation();
 
-  function navigateToDetail() {
+  function navigateToDetail(incident) {
     //Deve ter a mesma informação da name da rota
-    navigation.navigate('Detail');
+    navigation.navigate('Detail', { incident });
   }
 
   async function loadIncidents() {
-    const res = await api.get('incidents');
+    if(loading) {
+      return;
+    }
 
-    setIncidents(res.data);
+    if(total > 0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true);
+
+    const res = await api.get('incidents', {
+      params: { page }
+    });
+
+    setIncidents([...incidents, ...res.data]);
+    setTotal(res.headers['x-total-count']);
+    setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -33,7 +52,7 @@ export default function Incidents() {
       <View style={styles.header}>
         <Image source={logoImg} />
         <Text style={styles.headerText}>
-          Total de <Text style={styles.headerTextBold}>0 casos.</Text>
+          Total de <Text style={styles.headerTextBold}>{total} casos.</Text>
         </Text>
       </View>
       <Text style={styles.title}>Bem-vindo!</Text>
@@ -44,6 +63,8 @@ export default function Incidents() {
         style={styles.incidentList}
         keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
@@ -60,7 +81,7 @@ export default function Incidents() {
           }).format(incident.value)}
         </Text>
 
-            <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
+            <TouchableOpacity style={styles.detailsButton} onPress={() => navigateToDetail(incident)}>
               <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
               <Feather name="arrow-right" size={16} color="#e02041" />
             </TouchableOpacity>
