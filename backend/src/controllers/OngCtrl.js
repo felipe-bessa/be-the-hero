876984ec7c71d816e8707f13ee0dcfbd4be93697
Felipe Bessa <felipe-bessa@outlook.com>
2020-04-18 15:46:33 -0300

    Desacoplando a funcionalidade de geração de ID do controller de ONGs

--- backend/src/controllers/OngCtrl.js
+++ backend/src/controllers/OngCtrl.js
@@ -1,11 +1,11 @@
-const crypto = require('crypto');
+const generateUniqueID = require('../util/generateUniqueID');
 const connection = require('../database/connection');
 
 module.exports = {
   async create(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueID();
 
     await connection('ongs').insert({
       id,
